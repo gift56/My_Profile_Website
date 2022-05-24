ced6ca45bc5e2867dfd8986aab5910ff85d06c43
gift56 <efeasiughu@gmail.com>
2022-05-24 19:08:01 +0100

    updated typing script

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,4 +1,11 @@
 const switcherToggler = document.querySelector(".style-toggle");
+const dayNight = document.querySelector(".day-night");
+let typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Software Engineer", "Aspiring Full Stack", "It's Nice Meeting You..."],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
 
 switcherToggler.addEventListener("click", () => switchContent.classList.toggle("open"));
 
@@ -8,3 +15,15 @@ const closeSwitch = () => {
     }
 }
 window.addEventListener("scroll", closeSwitch);
+
+
+const darkMood = () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+}
+
+window.addEventListener("load", darkMood);
