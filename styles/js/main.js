8b1df0d36d53a59c73412d87364161dea310777b
gift56 <efeasiughu@gmail.com>
2022-05-24 17:49:09 +0100

    stying the spans color

---
+++ styles/js/main.js
@@ -0,0 +1,11 @@
+const switcherToggler = document.querySelector(".style-toggle");
+
+switcherToggler.addEventListener("click", () => switchContent.classList.toggle("open"));
+
+const closeSwitch = () => {
+    if (switchContent.classList.contains("open")) {
+        switchContent.classList.remove("open");
+    }
+}
+
+window.addEventListener("scroll", closeSwitch);
