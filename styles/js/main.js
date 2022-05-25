b890a1f1f40aff2a177a06df141cd860411699ea
gift56 <efeasiughu@gmail.com>
2022-05-25 13:26:29 +0100

    successfully added dark mood

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,7 +1,19 @@
 const switcherToggler = document.querySelector(".style-toggle");
 const dayNight = document.querySelector(".day-night");
+
+icons.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
+
+
 let typed = new Typed(".typing", {
-    strings: ["", "Web Designer", "Software Engineer", "Aspiring Full Stack", "It's Nice Meeting You..."],
+    strings: ["", "Web Designer", "Software Engineer", "Aspiring Full Stack", "It's Nice Meeting You."],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
